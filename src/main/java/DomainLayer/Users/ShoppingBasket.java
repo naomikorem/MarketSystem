2e1db8c544c22cae253787582073ef01efc774cd
R5-2\Administrator <jordan.rokach@gmail.com>
2022-04-20 21:34:01 +0300

    added user and shopping cart classes

--- src/main/java/DomainLayer/Users/ShoppingBasket.java
+++ src/main/java/DomainLayer/Users/ShoppingBasket.java
@@ -1,4 +1,29 @@
 package DomainLayer.Users;
 
+import DomainLayer.Stores.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingBasket {
+    private int storeId;
+    private List<Item> items;
+
+    public ShoppingBasket(int storeId) {
+        this.storeId = storeId;
+        this.items = new ArrayList<>();
+    }
+
+    public int getStoreId() {
+        return storeId;
+    }
+
+    public void addItem(Item item) {
+        this.items.add(item);
+    }
+
+    public void removeItem(Item item) {
+        this.items.remove(item);
+    }
+
 }
--- src/main/java/DomainLayer/Users/User.java
+++ src/main/java/DomainLayer/Users/User.java
@@ -1,4 +1,15 @@
 package DomainLayer.Users;
 
 public class User {
+    private UserState state;
+    private ShoppingCart shoppingCart;
+
+    public User(UserState state) {
+        this.state = state;
+        this.shoppingCart = new ShoppingCart();
+    }
+
+    public int getId() {
+        return state.getId();
+    }
 }
