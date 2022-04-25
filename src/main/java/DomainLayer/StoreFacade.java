11f4ee9727a0680ea78e46a60f3915de8ad635fe
naomikorem <63341850+naomikorem@users.noreply.github.com>
2022-04-25 18:39:30 +0300

    added invetory funcions (#22)

--- src/main/java/DomainLayer/StoreFacade.java
+++ src/main/java/DomainLayer/StoreFacade.java
@@ -6,7 +6,10 @@ import DomainLayer.Stores.StoreController;
 import DomainLayer.Users.User;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class StoreFacade {
 
@@ -16,7 +19,7 @@ public class StoreFacade {
         this.storeController = StoreController.getInstance();
     }
 
-    public void addNewStore(User owner , String storeName) {
+    public void addNewStore(User owner, String storeName) {
         if (!owner.isLoggedIn()) {
             throw new RuntimeException("The user is not logged in.");
         }
@@ -34,13 +37,44 @@ public class StoreFacade {
         s.addManager(owner.getName(), manager);
     }
 
-    public Response<Collection<Store>> getAllStores(){
+    public Response<Collection<Store>> getAllStores() {
         return new Response<>(storeController.getAllStores());
     }
-    public Response<Store> getStore(int id){
+
+    public Response<Store> getStore(int id) {
         return new Response<>(storeController.getStore(id));
     }
+
     public Response<Set<Item>> getItemsWithNameContains(String name) {
         return new Response<>(storeController.getItemsWithNameContains(name));
+    }
+
+    public Response<Set<Item>> getItemsWithKeyWord(String kw) {
+        return new Response<>(storeController.getItemsWithNameContains(kw));
+    }
+
+    public Response<Set<Item>> getItemsWithCategory(String category) {
+        try {
+            return new Response<>(storeController.getItemsWithNameContains(category));
+        } catch (Exception e) {
+            return new Response<>(e.getMessage());
+        }
+    }
+
+    public Response<Set<Item>> searchProducts(String productName, String category, List<String> keywords) {
+        try {
+            Set<Item> items = new HashSet<>();
+            if (!productName.isEmpty())
+                items = storeController.getItemsWithNameContains(productName);
+            if (!category.isEmpty())
+                items = storeController.getItemsWithNameContains(category).stream().filter(items::contains).collect(Collectors.toSet());
+            if (!keywords.isEmpty())
+                items = storeController.getItemsWithKeyWord(keywords).stream().filter(items::contains).collect(Collectors.toSet());
+            if (!items.isEmpty())
+                return new Response<>(items);
+            throw new Exception("No items mach the search");
+        } catch (Exception e) {
+            return new Response<>(e.getMessage());
+        }
     }
 }
