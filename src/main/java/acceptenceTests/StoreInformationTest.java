5c386edad95f7c62e3303cf65f5e8bfdde51f3aa
R5-2\Administrator <jordan.rokach@gmail.com>
2022-04-25 18:16:24 +0300

    Added test for retrieving store information

---
+++ src/main/java/acceptenceTests/StoreInformationTest.java
@@ -0,0 +1,46 @@
+package acceptenceTests;
+
+
+import DomainLayer.Response;
+import DomainLayer.Stores.Store;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+import static org.junit.Assert.*;
+
+public class StoreInformationTest extends AbstractTest {
+
+    private int storeId;
+    private String storeName;
+
+    public StoreInformationTest() {
+        super();
+    }
+
+    @Before
+    public void setup() {
+        bridge.enter();
+        bridge.register("user111@gmail.com", "user", "password");
+        bridge.login("user", "password");
+        this.storeName = "MyStore";
+        this.storeId = bridge.addNewStore(storeName).getObject().getStoreId();
+    }
+
+    @Test
+    public void testStoreInformationSuccess() {
+        Response<Store> res = bridge.getStoreInformation(storeId);
+        assertFalse(res.hadError());
+        assertEquals(res.getObject().getName(), this.storeName);
+        assertEquals(res.getObject().getStoreId(), this.storeId);
+    }
+
+    @Test
+    public void testStoreInformationFailure() {
+        assertTrue(bridge.getStoreInformation(-1).hadError());
+        assertTrue(bridge.getStoreInformation(storeId++).hadError());
+    }
+
+
+}
