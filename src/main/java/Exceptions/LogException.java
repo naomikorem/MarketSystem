bb7d405b1cb305e9f33b15ca2d7ab36accd3d52c
Jordan <jordan.rokach@gmail.com>
2022-05-07 16:27:06 +0300

    Changed some logs from info to warnings

--- src/main/java/Exceptions/LogException.java
+++ src/main/java/Exceptions/LogException.java
@@ -5,6 +5,6 @@ import Utility.LogUtility;
 public class LogException extends RuntimeException {
     public LogException(String name, String message) {
         super(name);
-        LogUtility.info(message);
+        LogUtility.warn(message);
     }
 }
