c1e6b9c36a214d70242a5e52f188ce8846adc4e9
sagi-brudni <brudnis@post.bgu.ac.il>
2022-05-16 16:52:17 +0300

    sign in sign up files

--- ClientFiles/src/App.js
+++ ClientFiles/src/App.js
@@ -12,30 +12,119 @@ function activateLasers() {
     stompClient.send('/app/market/login', {}, JSON.stringify({'user': "user", 'pass': "pass"}), 0)
     console.log("here");
 }
+
 function App() {
 
   return (
 
-    <div className="App">
-      <button onClick={activateLasers}>
-        Activate Lasers
-      </button>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          //href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router basename="/react-auth-ui/">
+          <div className="App">
+              <div className="appAside" />
+              <div className="appForm">
+                  <div className="pageSwitcher">
+                      <NavLink
+                          to="/sign-in"
+                          activeClassName="pageSwitcherItem-active"
+                          className="pageSwitcherItem"
+                      >
+                          Sign In
+                      </NavLink>
+                      <NavLink
+                          exact
+                          to="/"
+                          activeClassName="pageSwitcherItem-active"
+                          className="pageSwitcherItem"
+                      >
+                          Sign Up
+                      </NavLink>
+                  </div>
+
+                  <div className="formTitle">
+                      <NavLink
+                          to="/sign-in"
+                          activeClassName="formTitleLink-active"
+                          className="formTitleLink"
+                      >
+                          Sign In
+                      </NavLink>{" "}
+                      or{" "}
+                      <NavLink
+                          exact
+                          to="/"
+                          activeClassName="formTitleLink-active"
+                          className="formTitleLink"
+                      >
+                          Sign Up
+                      </NavLink>
+                  </div>
+
+                  <Route exact path="/" component={SignUpForm} />
+                  <Route path="/sign-in" component={SignInForm} />
+              </div>
+          </div>
+      </Router>
   );
 }
+
+
+/*import React, { Component } from "react";
+import { HashRouter as Router, Route, NavLink } from "react-router-dom";
+import SignUpForm from "./pages/SignUpForm";
+import SignInForm from "./pages/SignInForm";
+
+import "./App.css";
+
+class App extends Component {
+    render() {
+        return (
+            <Router basename="/react-auth-ui/">
+                <div className="App">
+                    <div className="appAside" />
+                    <div className="appForm">
+                        <div className="pageSwitcher">
+                            <NavLink
+                                to="/sign-in"
+                                activeClassName="pageSwitcherItem-active"
+                                className="pageSwitcherItem"
+                            >
+                                Sign In
+                            </NavLink>
+                            <NavLink
+                                exact
+                                to="/"
+                                activeClassName="pageSwitcherItem-active"
+                                className="pageSwitcherItem"
+                            >
+                                Sign Up
+                            </NavLink>
+                        </div>
+
+                        <div className="formTitle">
+                            <NavLink
+                                to="/sign-in"
+                                activeClassName="formTitleLink-active"
+                                className="formTitleLink"
+                            >
+                                Sign In
+                            </NavLink>{" "}
+                            or{" "}
+                            <NavLink
+                                exact
+                                to="/"
+                                activeClassName="formTitleLink-active"
+                                className="formTitleLink"
+                            >
+                                Sign Up
+                            </NavLink>
+                        </div>
+
+                        <Route exact path="/" component={SignUpForm} />
+                        <Route path="/sign-in" component={SignInForm} />
+                    </div>
+                </div>
+            </Router>
+        );
+    }
+}*/
 
 export default App;
